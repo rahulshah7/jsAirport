f9cf3d2ea102210e57c3bfc547b6f67ca647125a
Rahul Shah <rahulshah7@outlook.com>
2019-12-11 13:35:23 +0000

    Add Plane#get id()

--- src/plane.js
+++ src/plane.js
@@ -1,3 +1,9 @@
 module.exports = class Plane {
-  constructor() {}
+  constructor(id) {
+    this._id = id;
+  }
+
+  get id() {
+    return this._id;
+  }
 };
--- test/plane.test.js
+++ test/plane.test.js
@@ -1,9 +1,15 @@
 const Plane = require("../src/plane");
 
-beforeEach(() => (this.plane = new Plane()));
+beforeEach(() => (this.plane = new Plane("plane-0")));
 
 describe("Plane", () => {
   test("An Plane can be instantiated", () => {
     expect(this.plane).toBeInstanceOf(Plane);
+  });
+
+  describe("get id()", () => {
+    test("Returns the plane id", () => {
+      expect(this.plane.id).toBe("plane-0");
+    });
   });
 });
