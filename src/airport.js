46fe28c5b663e317058631f73fd0630a318400b4
Rahul Shah <rahulshah7@outlook.com>
2019-12-11 12:59:10 +0000

    Add Airport#land() method

--- src/airport.js
+++ src/airport.js
@@ -1,3 +1,13 @@
 module.exports = class Airport {
-  constructor() {}
+  constructor() {
+    this._planes = [];
+  }
+
+  get planes() {
+    return this._planes;
+  }
+
+  land(plane) {
+    this._planes.push(plane);
+  }
 };
--- test/airport.test.js
+++ test/airport.test.js
@@ -1,11 +1,26 @@
 const Airport = require("../src/airport");
+const Plane = require("../src/plane");
+jest.mock("../src/plane");
 
 let airport;
+let plane;
 
-beforeEach(() => (airport = new Airport()));
+beforeEach(() => {
+  airport = new Airport();
+  Plane.mockClear();
+  plane = new Plane();
+});
 
 describe("Airport", () => {
   test("An Airport can be instantiated", () => {
     expect(airport).toBeInstanceOf(Airport);
+  });
+
+  describe("land(plane)", () => {
+    test("Landing a plane adds 1 plane to the airport", () => {
+      expect(airport.planes.length).toEqual(0);
+      airport.land(plane);
+      expect(airport.planes.length).toEqual(1);
+    });
   });
 });
